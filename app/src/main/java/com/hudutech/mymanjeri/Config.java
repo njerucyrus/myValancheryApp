271ff71bf0c1d5e0340abb68c2bd4eadf42c6868
njerucyrus <njerucyrus5@gmail.com>
2018-07-06 21:01:51 +0300

    added mp and mla

--- app/src/main/java/com/hudutech/mymanjeri/Config.java
+++ app/src/main/java/com/hudutech/mymanjeri/Config.java
@@ -1,9 +1,21 @@
 package com.hudutech.mymanjeri;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.support.v7.app.AppCompatActivity;
 
-public class Config {
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+@SuppressLint("Registered")
+public class Config extends AppCompatActivity{
 
     public static boolean isAdmin(Context mContext) {
         SharedPreferences sharedPrefs = mContext.getSharedPreferences("AUTH_DATA",
@@ -16,6 +28,17 @@ public class Config {
                 Context.MODE_PRIVATE);
         return sharedPrefs.getBoolean("isSBAdmin", false);
     }
+
+
+    public static Bitmap getBitmapFromUri(Context mContext, Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                mContext.getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
 
 
 
